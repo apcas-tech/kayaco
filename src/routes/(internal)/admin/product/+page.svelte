db77757c2c2281d1010dc1657a68550e6f493ef2
paul allan palacio <palaciopaulbermudez@gmail.com>
2025-04-25 16:48:10 +0800

    fix add product form

--- src/routes/(internal)/admin/product/+page.svelte
+++ src/routes/(internal)/admin/product/+page.svelte
@@ -1,42 +1,139 @@
 <script lang="ts">
-  import * as Dialog from "$lib/components/ui/dialog/index.js";
-  import { Input } from "$lib/components/ui/input";
-  import { Label } from "$lib/components/ui/label";
-  import { Button } from "$lib/components/ui/button";
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { Input } from '$lib/components/ui/input';
+	import * as Form from '$lib/components/ui/form';
+	import { productSchema } from '$lib/zod-schema';
+	import { superForm } from 'sveltekit-superforms';
+	import { zodClient, zod } from 'sveltekit-superforms/adapters';
+	import Label from '$lib/components/ui/label/label.svelte';
+	import { LoaderCircle } from '@lucide/svelte';
+	import { toast } from 'svelte-sonner';
+	import { defaults } from 'sveltekit-superforms';
+	import { Button } from '$lib/components/ui/button';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
 
-  let name = "";
-  let email = "";
+	let open = $state(false);
 
-  const handleSubmit = (e: Event) => {
-    e.preventDefault();
-    console.log({ name, email });
- 
-  };
+	const form = superForm(defaults(zod(productSchema)), {
+		validators: zodClient(productSchema),
+		resetForm: false,
+		async onResult({ result }) {
+			if (result.type === 'failure') {
+				toast.error('Failed to create product.');
+			} else if (result.type === 'success') {
+				toast.success('Product created successfully!');
+				open = false;
+			}
+		}
+	});
+
+	const { form: formData, enhance, submitting, submit } = form;
 </script>
 
-<Dialog.Root>
-  <Dialog.Trigger>
-    <Button>Open Form</Button>
-  </Dialog.Trigger>
+<Dialog.Root bind:open>
+	<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>Add Product</Dialog.Trigger>
 
-  <Dialog.Content class="space-y-4">
-    <Dialog.Header>
-      <Dialog.Title>Submit Your Info</Dialog.Title>
-      <Dialog.Description>Please enter your name and email below.</Dialog.Description>
-    </Dialog.Header>
+	<Dialog.Content class="space-y-4">
+		<Dialog.Header>
+			<Dialog.Title>Add Product</Dialog.Title>
+			<Dialog.Description>Enter the product info below</Dialog.Description>
+		</Dialog.Header>
 
-    <form on:submit|preventDefault={handleSubmit} class="space-y-4">
-      <div>
-        <Label for="name">Name</Label>
-        <Input id="name" bind:value={name} placeholder="Enter your name" required />
-      </div>
+		<form
+			method="POST"
+			action="?/product"
+			use:enhance
+			class="w-full gap-y-12 space-y-6"
+			enctype="multipart/form-data"
+		>
+			<div class="space-y-4">
+				<Form.Field {form} name="description">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Label>Product Name</Label>
+							<Input
+								placeholder="Enter Product Description"
+								type="text"
+								{...props}
+								bind:value={$formData.description}
+							/>
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
 
-      <div>
-        <Label for="email">Email</Label>
-        <Input id="email" type="email" bind:value={email} placeholder="Enter your email" required />
-      </div>
+				<Form.Field {form} name="image_url">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Label>Product Image</Label>
+							<Input
+								placeholder="Enter Product Image"
+								type="text"
+								{...props}
+								bind:value={$formData.image_url}
+							/>
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
 
-      <Button type="submit" class="w-full">Submit</Button>
-    </form>
-  </Dialog.Content>
+				<Form.Field {form} name="product_name">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Label>Product Name</Label>
+							<Input
+								placeholder="Enter Product Name"
+								type="text"
+								{...props}
+								bind:value={$formData.product_name}
+							/>
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+
+				<Form.Field {form} name="price">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Label>Product Price</Label>
+							<Input
+								placeholder="Enter Product Price"
+								type="number"
+								{...props}
+								bind:value={$formData.price}
+							/>
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+
+				<Form.Field {form} name="quantity">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Label>Product Quantity</Label>
+							<Input
+								placeholder="Enter Product Quantity"
+								type="number"
+								{...props}
+								bind:value={$formData.quantity}
+							/>
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+			</div>
+			<Dialog.Footer class="mt-8 gap-4 md:gap-0">
+				<Button variant="outline" disabled={$submitting} onclick={() => (open = false)}>
+					Cancel
+				</Button>
+				<Form.Button class=" bg-primary" disabled={$submitting} onclick={() => submit()}>
+					{#if $submitting}
+						<LoaderCircle class="mr-1 size-4 animate-spin" /> Creating Product...
+					{:else}
+						Create Product
+					{/if}
+				</Form.Button>
+			</Dialog.Footer>
+		</form>
+	</Dialog.Content>
 </Dialog.Root>
